1cbec712b125c89cc96ae1b284a89a0f93146f97
ErianSerna <erianserna1114297@correo.itm.edu.co>
2025-06-03 00:40:04 -0500

    Cambios

---
+++ Paginas/Scripts/Inventario.js
@@ -0,0 +1,129 @@
+﻿var URLBase = "http://localhost:55534/";
+
+jQuery(function () {
+    $("#dvMenu").load("../Paginas/Menu.html");
+    LlenarTablaInventario();
+    RegistrarEventos();
+});
+
+function RegistrarEventos() {
+    $("#btnInsertar").click(function (e) {
+        e.preventDefault();
+        InsertarInventario();
+    });
+
+    $("#btnActualizar").click(function (e) {
+        e.preventDefault();
+        ActualizarInventario();
+    });
+
+    $("#btnEliminar").click(function (e) {
+        e.preventDefault();
+        EliminarInventario();
+    });
+
+    $("#btnConsultar").click(function (e) {
+        e.preventDefault();
+        LlenarTablaInventario();
+    });
+}
+
+function LlenarTablaInventario() {
+    let URL = URLBase + "api/Inventario/ConsultarTodos";
+
+    $.getJSON(URL, function (data) {
+        $("#tblInventario tbody").empty();
+
+        data.forEach(function (inv) {
+            let fila = `
+                <tr>
+                    <td><button class="btn btn-warning btn-sm" onclick='CargarDatos(${JSON.stringify(JSON.stringify(inv))})'>Editar</button></td>
+                    <td>${inv.Id}</td>
+                    <td>${inv.Stock}</td>
+                    <td>${inv.UltimoRestock}</td>
+                    <td>${inv.IdProducto}</td>
+                    <td>${inv.IdSede}</td>
+                </tr>`;
+            $("#tblInventario tbody").append(fila);
+        });
+    });
+}
+
+function CargarDatos(jsonString) {
+    let inv = JSON.parse(JSON.parse(jsonString));
+    $("#txtIdInventario").val(inv.Id);
+    $("#txtStock").val(inv.Stock);
+    $("#txtUltimoRestock").val(inv.UltimoRestock);
+    $("#ddlProducto").val(inv.IdProducto);
+    $("#ddlSede").val(inv.IdSede);
+}
+
+function InsertarInventario() {
+    let inventario = ObtenerDatosFormulario();
+
+    $.ajax({
+        type: "POST",
+        url: URLBase + "api/Inventario/Insertar",
+        data: JSON.stringify(inventario),
+        contentType: "application/json",
+        success: function () {
+            alert("Inventario insertado con éxito");
+            LlenarTablaInventario();
+            LimpiarFormulario();
+        }
+    });
+}
+
+function ActualizarInventario() {
+    let inventario = ObtenerDatosFormulario();
+
+    $.ajax({
+        type: "PUT",
+        url: URLBase + "api/Inventario/Actualizar",
+        data: JSON.stringify(inventario),
+        contentType: "application/json",
+        success: function () {
+            alert("Inventario actualizado con éxito");
+            LlenarTablaInventario();
+            LimpiarFormulario();
+        }
+    });
+}
+
+function EliminarInventario() {
+    let id = $("#txtIdInventario").val();
+    if (!id) {
+        alert("Selecciona un registro para eliminar");
+        return;
+    }
+
+    $.ajax({
+        type: "DELETE",
+        url: URLBase + "api/Inventario/Eliminar/" + id,
+        success: function () {
+            alert("Inventario eliminado con éxito");
+            LlenarTablaInventario();
+            LimpiarFormulario();
+        }
+    });
+}
+
+function ObtenerDatosFormulario() {
+    return {
+        Id: $("#txtIdInventario").val() || 0,
+        Stock: $("#txtStock").val(),
+        UltimoRestock: $("#txtUltimoRestock").val(),
+        IdProducto: $("#ddlProducto").val(),
+        IdSede: $("#ddlSede").val()
+    };
+}
+
+function LimpiarFormulario() {
+    $("#txtIdInventario").val('');
+    $("#txtStock").val('');
+    $("#txtUltimoRestock").val('');
+    $("#ddlProducto").val('');
+    $("#ddlSede").val('');
+}
+
+
--- Paginas/Scripts/Usuario.js
+++ Paginas/Scripts/Usuario.js
@@ -1,58 +1,74 @@
-﻿//var URLBase = "http://itm20251jue1820.runasp.net/";
-var URLBase = "http://localhost:55534/";
+﻿var URLBase = "http://localhost:55534/"; // para el consumo de las apis
 
-jQuery(function () {
-    //Registrar los botones para responder al evento click
-    $("#dvMenu").load("../Paginas/Menu.html");
-    LlenarTablaUsuarios();
-});
-function LlenarTablaUsuarios() {
-    let URL = URLBase + "api/Empleados/ConsultarTodos";
-    LlenarTablaXServiciosAuth(URL, "#tblEmpleados");
-}
-async function EjecutarComando(Metodo, Funcion) {
-    let URL = URLBase + "api/Empleados/" + Funcion;
-    const empleado = new Empleado($("#txtDocumento").val(), $("#txtNombre").val(), $("#txtPrimerApellido").val(),
-        $("#txtSegundoApellido").val(), $("#txtDireccion").val(), $("#txtTelefono").val(), $("#txtFechaNacimiento").val());
-    const rpta = await EjecutarComandoServicioAuth(Metodo, URL, empleado);
-    LlenarTablaEmpleados();
-}
-async function Consultar() {
-    let Documento = $("#txtDocumento").val();
-    let URL = URLBase + "api/Empleados/ConsultarXDocumento?Documento=" + Documento;
-    const empleado = await ConsultarServicioAuth(URL);
-    if (empleado == null || empleado == undefined) {
-        $("#dvMensaje").removeClass("alert alert-success");
-        $("#dvMensaje").addClass("alert alert-danger");
-        $("#dvMensaje").html("No se pudo realizar la consulta del empleado");
-        $("#txtNombre").val("");
-        $("#txtPrimerApellido").val("");
-        $("#txtSegundoApellido").val("");
-        $("#txtFechaNacimiento").val("");
-        $("#txtTelefono").val("");
-        $("#txtDireccion").val("");
+document.addEventListener("DOMContentLoaded", function () {
+    // Metodos y demas
+    llenarTabla();
+
+    // Funcion para llenar la tabla Usuarios
+    function llenarTabla() {
+        $.ajax({
+            type: "GET",
+            url: URLBase + "api/Usuario/Listar",
+            success: function (response) {
+                console.log(response) // Depuracion
+            }
+        })
     }
-    else {
-        $("#dvMensaje").removeClass("alert alert-danger");
-        $("#dvMensaje").addClass("alert alert-success");
-        $("#dvMensaje").html("");
-        //Consultó el empleado
-        $("#txtNombre").val(empleado.Nombre);
-        $("#txtPrimerApellido").val(empleado.PrimerApellido);
-        $("#txtSegundoApellido").val(empleado.SegundoApellido);
-        $("#txtFechaNacimiento").val(empleado.FechaNacimiento.split('T')[0]);
-        $("#txtTelefono").val(empleado.Telefono);
-        $("#txtDireccion").val(empleado.Direccion);
-    }
-}
-class Empleado {
-    constructor(Documento, Nombre, PrimerApellido, SegundoApellido, Direccion, Telefono, FechaNacimiento) {
-        this.Documento = Documento;
-        this.Nombre = Nombre;
-        this.PrimerApellido = PrimerApellido;
-        this.SegundoApellido = SegundoApellido;
-        this.Direccion = Direccion;
-        this.Telefono = Telefono;
-        this.FechaNacimiento = FechaNacimiento;
-    }
-}
+
+
+})
+
+//jQuery(function () {
+//    //Registrar los botones para responder al evento click
+//    $("#dvMenu").load("../Paginas/Menu.html");
+//    LlenarTablaUsuarios();
+//});
+//function LlenarTablaUsuarios() {
+//    let URL = URLBase + "api/Usuario/Listar";
+//    LlenarTablaXServiciosAuth(URL, "#tblUsuario");
+//}
+//async function EjecutarComando(Metodo, Funcion) {
+//    let URL = URLBase + "api/Usuario/" + Funcion;
+//    const usuario = new Usuario($("#txtNombreUsuario").val(), $("#txtDocumentoUsuario").val(),$("#").val(),);
+//    const rpta = await EjecutarComandoServicioAuth(Metodo, URL, empleado);
+//    LlenarTablaEmpleados();
+//}
+//async function Consultar() {
+//    let Documento = $("#txtDocumento").val();
+//    let URL = URLBase + "api/Empleados/ConsultarXDocumento?Documento=" + Documento;
+//    const empleado = await ConsultarServicioAuth(URL);
+//    if (empleado == null || empleado == undefined) {
+//        $("#dvMensaje").removeClass("alert alert-success");
+//        $("#dvMensaje").addClass("alert alert-danger");
+//        $("#dvMensaje").html("No se pudo realizar la consulta del empleado");
+//        $("#txtNombre").val("");
+//        $("#txtPrimerApellido").val("");
+//        $("#txtSegundoApellido").val("");
+//        $("#txtFechaNacimiento").val("");
+//        $("#txtTelefono").val("");
+//        $("#txtDireccion").val("");
+//    }
+//    else {
+//        $("#dvMensaje").removeClass("alert alert-danger");
+//        $("#dvMensaje").addClass("alert alert-success");
+//        $("#dvMensaje").html("");
+//        //Consultó el empleado
+//        $("#txtNombre").val(empleado.Nombre);
+//        $("#txtPrimerApellido").val(empleado.PrimerApellido);
+//        $("#txtSegundoApellido").val(empleado.SegundoApellido);
+//        $("#txtFechaNacimiento").val(empleado.FechaNacimiento.split('T')[0]);
+//        $("#txtTelefono").val(empleado.Telefono);
+//        $("#txtDireccion").val(empleado.Direccion);
+//    }
+//}
+//class Empleado {
+//    constructor(Documento, Nombre, PrimerApellido, SegundoApellido, Direccion, Telefono, FechaNacimiento) {
+//        this.Documento = Documento;
+//        this.Nombre = Nombre;
+//        this.PrimerApellido = PrimerApellido;
+//        this.SegundoApellido = SegundoApellido;
+//        this.Direccion = Direccion;
+//        this.Telefono = Telefono;
+//        this.FechaNacimiento = FechaNacimiento;
+//    }
+//}
