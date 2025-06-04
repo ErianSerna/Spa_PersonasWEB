21a129750fd77e014518b0d456b83490b799949e
ErianSerna <erianserna1114297@correo.itm.edu.co>
2025-06-04 03:32:38 -0500

    Creación del CRUD y de las vistas de inventarios

--- Paginas/Scripts/Inventario.js
+++ Paginas/Scripts/Inventario.js
@@ -1,129 +1,380 @@
-﻿var URLBase = "http://localhost:55534/";
+﻿var URLBase = "http://spapersonas2025.runasp.net/";
 
-jQuery(function () {
-    $("#dvMenu").load("../Paginas/Menu.html");
-    LlenarTablaInventario();
-    RegistrarEventos();
-});
+document.addEventListener("DOMContentLoaded", function () {
 
-function RegistrarEventos() {
-    $("#btnInsertar").click(function (e) {
-        e.preventDefault();
-        InsertarInventario();
-    });
+    LlenarDDLProductos();
 
-    $("#btnActualizar").click(function (e) {
-        e.preventDefault();
-        ActualizarInventario();
-    });
+    LlenarDDLSedes();
 
-    $("#btnEliminar").click(function (e) {
-        e.preventDefault();
-        EliminarInventario();
-    });
+    llenarTabla();
 
-    $("#btnConsultar").click(function (e) {
-        e.preventDefault();
-        LlenarTablaInventario();
-    });
-}
+    // ----------------- LISTAR -----------------
+    function llenarTabla() {
+        const token2 = getCookie("token");
+        $.ajax({
+            type: "GET",
+            url: URLBase + "api/Inventario/ConsultarTodos",
+            headers: {
+                'Authorization': 'Bearer ' + token2
+            },
+            success: function (response) {
+                let htmlContent = "";
+                let tablaBody = document.getElementById("tblInventarioBody");
+                response.forEach(inv => {
+                    htmlContent += `
+                    <tr>
+                        <td>${inv.Id}</td>
+                        <td>${inv.Stock}</td>
+                        <td>${inv.UltimoRestock}</td>
+                        <td>${inv.IdProducto}</td>
+                        <td>${inv.IdSede}</td>
+                        <td>
+                            <button class="btn btn-sm btn-primary w-100 btnActualizar" data-id=${inv.Id}>Actualizar</button>
+                            <button class="btn btn-sm btn-danger w-100 btnEliminar" data-id=${inv.Id}>Eliminar</button>
+                        </td>
+                    </tr>
+                `;
+                });
 
-function LlenarTablaInventario() {
-    let URL = URLBase + "api/Inventario/ConsultarTodos";
-
-    $.getJSON(URL, function (data) {
-        $("#tblInventario tbody").empty();
-
-        data.forEach(function (inv) {
-            let fila = `
-                <tr>
-                    <td><button class="btn btn-warning btn-sm" onclick='CargarDatos(${JSON.stringify(JSON.stringify(inv))})'>Editar</button></td>
-                    <td>${inv.Id}</td>
-                    <td>${inv.Stock}</td>
-                    <td>${inv.UltimoRestock}</td>
-                    <td>${inv.IdProducto}</td>
-                    <td>${inv.IdSede}</td>
-                </tr>`;
-            $("#tblInventario tbody").append(fila);
+                tablaBody.innerHTML = htmlContent;
+            },
+            error: function (xhr, status, error) {
+                console.error("Error en la solicitud:", xhr.responseText);
+            }
         });
-    });
-}
-
-function CargarDatos(jsonString) {
-    let inv = JSON.parse(JSON.parse(jsonString));
-    $("#txtIdInventario").val(inv.Id);
-    $("#txtStock").val(inv.Stock);
-    $("#txtUltimoRestock").val(inv.UltimoRestock);
-    $("#ddlProducto").val(inv.IdProducto);
-    $("#ddlSede").val(inv.IdSede);
-}
-
-function InsertarInventario() {
-    let inventario = ObtenerDatosFormulario();
-
-    $.ajax({
-        type: "POST",
-        url: URLBase + "api/Inventario/Insertar",
-        data: JSON.stringify(inventario),
-        contentType: "application/json",
-        success: function () {
-            alert("Inventario insertado con éxito");
-            LlenarTablaInventario();
-            LimpiarFormulario();
-        }
-    });
-}
-
-function ActualizarInventario() {
-    let inventario = ObtenerDatosFormulario();
-
-    $.ajax({
-        type: "PUT",
-        url: URLBase + "api/Inventario/Actualizar",
-        data: JSON.stringify(inventario),
-        contentType: "application/json",
-        success: function () {
-            alert("Inventario actualizado con éxito");
-            LlenarTablaInventario();
-            LimpiarFormulario();
-        }
-    });
-}
-
-function EliminarInventario() {
-    let id = $("#txtIdInventario").val();
-    if (!id) {
-        alert("Selecciona un registro para eliminar");
-        return;
     }
 
-    $.ajax({
-        type: "DELETE",
-        url: URLBase + "api/Inventario/Eliminar/" + id,
-        success: function () {
-            alert("Inventario eliminado con éxito");
-            LlenarTablaInventario();
-            LimpiarFormulario();
+    // ----------------- LISTAR -----------------
+
+    // ----------------- INSERTAR -----------------
+
+    // Metodo para insertar un inventario a la bd
+    function insertarUsuario() {
+        // Obtener los datos del formulario
+        let stock = ($("#txtStock").val() || "").trim();
+        let ultimorestock = ($("#txtUltimoRestock").val() || "").trim();
+        let idProducto = ($("#ddlProducto").val() || "").trim();
+        let idSede = ($("#ddlSede").val() || "").trim();
+        const token2 = getCookie("token");
+
+        if (!stock || !ultimorestock || !idProducto || !idSede) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Los campos deben llenarse",
+                timer: 2000
+            });
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: URLBase + "api/Inventario/Insertar",
+            headers: {
+                'Authorization': 'Bearer ' + token2
+            },
+            data: {
+                "Stock": stock,
+                "UltimoRestock": ultimorestock,
+                "IdProducto": idProducto,
+                "IdSede": idSede
+            },
+
+            success: function (response) {
+                console.log(response);
+                Swal.fire({
+                    icon: "success",
+                    title: "Inserción Exitosa",
+                    text: "Se ha insertado correctamente el inventario",
+                    timer: 2000
+                });
+
+                llenarTabla();
+            }
+        })
+    }
+
+    document.getElementById("btnInsertar").addEventListener("click", function (e) {
+        e.preventDefault();
+        insertarUsuario();
+    })
+
+
+    // ----------------- FIN INSERTAR -----------------
+
+
+    // ----------------- ACTUALIZAR -----------------
+
+    // Metodo para actualizar un usuario
+    function actualizarInventario(id) {
+        const token2 = getCookie("token");
+        $.ajax({
+            type: "GET",
+            url: URLBase + "api/Inventario/ConsultarXId",
+            headers: {
+                'Authorization': 'Bearer ' + token2
+            },
+            data: {
+                "idInventario": id
+            },
+            success: function (response) {
+                console.log(response);
+                $("#txtIdInventario").val(response.Id);
+                $("#txtStock").val(response.Stock);
+                $("#txtUltimoRestock").val(response.UltimoRestock);
+                $("#ddlProducto").val(response.IdProducto);
+                $("#ddlSede").val(response.IdSede);
+            }
+        })
+    }
+
+    document.addEventListener("click", function (e) {
+        if (e.target.classList.contains("btnActualizar")) { // Actualizar
+            e.preventDefault();
+            const id = e.target.getAttribute("data-id");
+            console.log("Actualizar ID:", id);
+            $("#btnInsertar").prop("disabled", true);
+            $("#btnConsultar").prop("disabled", true);
+            $("#btnConfirmar").removeAttr("disabled");
+            $("#btnCancelar").removeAttr("disabled");
+            actualizarInventario(id);
+
         }
     });
-}
-
-function ObtenerDatosFormulario() {
-    return {
-        Id: $("#txtIdInventario").val() || 0,
-        Stock: $("#txtStock").val(),
-        UltimoRestock: $("#txtUltimoRestock").val(),
-        IdProducto: $("#ddlProducto").val(),
-        IdSede: $("#ddlSede").val()
-    };
-}
-
-function LimpiarFormulario() {
-    $("#txtIdInventario").val('');
-    $("#txtStock").val('');
-    $("#txtUltimoRestock").val('');
-    $("#ddlProducto").val('');
-    $("#ddlSede").val('');
-}
 
 
+    // Metodo para cuando se va a confirmar la actualizacion del usuario
+    function confirmarActualizacionInventario() {
+        // Obtener los datos del formulario
+        let stock = ($("#txtStock").val() || "").trim();
+        let ultimorestock = ($("#txtUltimoRestock").val() || "").trim();
+        let idProducto = ($("#ddlProducto").val() || "").trim();
+        let idSede = ($("#ddlSede").val() || "").trim();
+        let id = ($("#txtIdInventario").val() || "").trim();
+        const token2 = getCookie("token");
+
+        if (!stock || !ultimorestock || !idProducto || !idSede || !id) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Los campos deben llenarse",
+                timer: 2000
+            });
+            return;
+        }
+        $.ajax({
+            type: "PUT",
+            url: URLBase + "api/Inventario/Actualizar",
+            headers: {
+                'Authorization': 'Bearer ' + token2
+            },
+            data: {
+                "Id": id,
+                "Stock": stock,
+                "UltimoRestock": ultimorestock,
+                "IdProducto": idProducto,
+                "IdSede": idSede
+            },
+            success: function (response) {
+                console.log(response);
+                // Reactivar o desactivar los botones segun sea el caso
+                $("#btnInsertar").removeAttr("disabled");
+                $("#btnConsultar").removeAttr("disabled");
+                $("#btnConfirmar").prop("disabled", true);
+                $("#btnCancelar").prop("disabled", true);
+
+                Swal.fire({
+                    icon: 'success',
+                    title: '¡Usuario actualizado!',
+                    text: 'El inventario se actualizó con éxito.',
+                    confirmButtonText: 'Aceptar'
+                });
+
+                $("#txtIdInventario").val("");
+                $("#txtStock").val("");
+                $("#txtUltimoRestock").val("");
+                $("#ddlProducto").val("");
+                $("#ddlSede").val("");
+                $("#txtId").val("");
+                llenarTabla();
+
+            }
+
+        })
+    }
+
+    // Evento para detectar click en confirmar al querer actualizar el usuario
+    document.getElementById("btnConfirmar").addEventListener("click", function (e) {
+        e.preventDefault();
+        confirmarActualizacionInventario();
+    })
+
+    // Evento para detectar click en cancelar al querer actualizar el usuario
+    document.getElementById("btnCancelar").addEventListener("click", function (e) {
+        e.preventDefault();
+        $("#txtIdInventario").val("");
+        $("#txtStock").val("");
+        $("#txtUltimoRestock").val("");
+        $("#ddlProducto").val("");
+        $("#ddlSede").val("");
+    });
+
+
+    // ----------------- FIN ACTUALIZAR -----------------
+
+
+    // ----------------- ELIMINAR -----------------
+
+    function eliminarInventario(id) {
+        const token2 = getCookie("token");
+        $.ajax({
+            type: "DELETE",
+            url: URLBase + "api/Inventario/Eliminar",
+            headers: {
+                'Authorization': 'Bearer ' + token2,
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify({ Id: id }),
+            success: function (response) {
+                Swal.fire({
+                    icon: "success",
+                    title: "Eliminación Exitosa",
+                    text: "Se ha eliminado correctamente el inventario",
+                    timer: 2000
+                });
+                llenarTabla();
+            }
+        });
+
+    }
+
+    document.addEventListener("click", function (e) {
+        if (e.target.classList.contains("btnEliminar")) { // Eliminar
+            e.preventDefault();
+
+            Swal.fire({
+                title: "¿Está seguro de eliminar el inventario?",
+                showCancelButton: true,
+                confirmButtonText: "Eliminar",
+                denyButtonText: `Cancelar`
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    const id = e.target.getAttribute("data-id");
+                    eliminarInventario(id);
+                }
+            });
+        }
+
+    });
+
+    // ----------------- FIN ELIMINAR -----------------
+
+    // ----------------- CONSULTAR --------------------
+
+    // Metodo para consultarPorId
+    function consultarPorId(id) {
+        const token2 = getCookie("token");
+        $.ajax({
+            type: "GET",
+            url: URLBase + "api/Inventario/ConsultarXId",
+            headers: {
+                'Authorization': 'Bearer ' + token2
+            },
+            data: {
+                "idInventario": id
+            },
+            success: function (response) {
+                console.log(response);
+                if (response && typeof response === "object") {
+                    let htmlContent = "";
+                    let tablaBody = document.getElementById("tblInventarioBody");
+                    htmlContent += `
+                    <tr>
+                        <td>${response.Id}</td>
+                        <td>${response.Stock}</td>
+                        <td>${response.UltimoRestock}</td>
+                        <td>${response.IdProducto}</td>
+                        <td>${response.IdSede}</td>
+                        <td>
+                            <button class="btn btn-sm btn-primary w-100 btnActualizar" data-id=${response.Id}>Actualizar</button>
+                            <button class="btn btn-sm btn-danger w-100 btnEliminar" data-id=${response.Id}>Eliminar</button>
+                        </td>
+                    </tr>
+                `;
+
+                    tablaBody.innerHTML = htmlContent;
+
+                } else {
+                    Swal.fire({
+                        icon: "warning",
+                        toast: true,
+                        position: "top-end",
+                        title: "Inventario no encontrado",
+                        timer: 2000
+                    });
+                }
+
+            }
+        })
+    }
+
+
+    // Evento que detecta cuando sea desea consultar por cedula
+    document.getElementById("btnConsultar").addEventListener("click", function (e) {
+        e.preventDefault();
+        let textoBuscar = ($("#txtBuscar").val() || "").trim();
+
+        console.log(textoBuscar);
+        if (textoBuscar) {
+            consultarPorId(textoBuscar);
+        } else {
+            llenarTabla();
+        }
+    })
+
+    //-------------- FIN CONSULTAR -----------------
+
+
+    //--------------------------------------------------
+
+    function LlenarDDLProductos() {
+        const token2 = getCookie("token");
+
+        $.ajax({
+            type: "GET",
+            url: URLBase + "api/Producto/ConsultarTodos",
+            headers: { 'Authorization': 'Bearer ' + token2 },
+            success: function (data) {
+                let ddl = $("#ddlProducto");
+                ddl.empty();
+                ddl.append('<option value="">Seleccione un producto</option>');
+                data.forEach(p => {
+                    ddl.append(`<option value="${p.Id}">${p.Nombre}</option>`);
+                });
+            },
+            error: function (xhr) {
+                console.error("Error al cargar productos:", xhr.responseText);
+            }
+        });
+    }
+
+    function LlenarDDLSedes() {
+        const token2 = getCookie("token");
+        $.ajax({
+            type: "GET",
+            url: URLBase + "api/Sede/ConsultarTodos",
+            headers: { 'Authorization': 'Bearer ' + token2 },
+            success: function (data) {
+                let ddl = $("#ddlSede");
+                ddl.empty();
+                ddl.append('<option value="">Seleccione una sede</option>');
+                data.forEach(s => {
+                    ddl.append(`<option value="${s.Id}">${s.Nombre}</option>`);
+                });
+            },
+            error: function (xhr) {
+                console.error("Error al cargar sedes:", xhr.responseText);
+            }
+        });
+    }
+});
